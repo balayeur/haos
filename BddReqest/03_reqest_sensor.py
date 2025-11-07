d88e11030a6c4ccf915d1691870a498c81b90089
balayeur <36159749+balayeur@users.noreply.github.com>
2025-11-07 22:41:15 +0100

    Update 03_reqest_sensor.py

--- BddReqest/03_reqest_sensor.py
+++ BddReqest/03_reqest_sensor.py
@@ -1,6 +1,18 @@
 import requests
+from config import CONFIG
 
+url = CONFIG['ha_url'] + "/api/states/sensor.burnerroom_boiler_temperature"
+token = CONFIG['ha_token']
+headers = {
+    "Authorization": f"Bearer {token}",
+    "Content-Type": "application/json",
+}
 
+# url = "http://192.168.1.36:8123/api/states/sensor.burnerroom_boiler_temperature"
+# headers = {
+#     "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiIzYWVhZGU0N2I2Y2Q0MTVmYThmYTE1MWJiMTQ4ZTRlYSIsImlhdCI6MTc2MjUyNDUzNiwiZXhwIjoyMDc3ODg0NTM2fQ.M6Ue9VoJDCKAGG57IDwLjyonRFOeI1frocJIQ9UsF4Y",
+#     "Content-Type": "application/json",
+# }
 
 try:
     response = requests.get(url, headers=headers)
