97c462afade0e8c7593523d5d36f889049f13b7c
balayeur <36159749+balayeur@users.noreply.github.com>
2025-11-07 22:38:25 +0100

    Create 02_reqest_post.py

---
+++ BddReqest/02_reqest_post.py
@@ -0,0 +1,26 @@
+import requests
+import json
+
+
+# Данные в теле запроса
+data = {
+    "filter_entity_id": "sensor.burnerroom_boiler_temperature",
+    "start_time": "2023-12-01T00:00:00+00:00",
+    "end_time": "2023-12-01T23:59:59+00:00",
+    "minimal_response": True
+}
+
+try:
+    response = requests.post(url, headers=headers, json=data)
+    print(f"Status Code: {response.status_code}")
+    
+    if response.status_code == 200:
+        result = response.json()
+        print("Успех!")
+        for entry in result[0]:
+            print(f"{entry['last_updated']}: {entry['state']}")
+    else:
+        print(f"Ошибка: {response.text}")
+        
+except Exception as e:
+    print(f"Ошибка: {e}")
