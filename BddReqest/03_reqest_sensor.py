a3e430ce01308d35e0d254ef2f35c44a233e4a02
balayeur <36159749+balayeur@users.noreply.github.com>
2025-11-07 22:44:41 +0100

    Update 03_reqest_sensor.py

--- BddReqest/03_reqest_sensor.py
+++ BddReqest/03_reqest_sensor.py
@@ -8,12 +8,6 @@ headers = {
     "Content-Type": "application/json",
 }
 
-# url = "http://192.168.1.36:8123/api/states/sensor.burnerroom_boiler_temperature"
-# headers = {
-#     "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiIzYWVhZGU0N2I2Y2Q0MTVmYThmYTE1MWJiMTQ4ZTRlYSIsImlhdCI6MTc2MjUyNDUzNiwiZXhwIjoyMDc3ODg0NTM2fQ.M6Ue9VoJDCKAGG57IDwLjyonRFOeI1frocJIQ9UsF4Y",
-#     "Content-Type": "application/json",
-# }
-
 try:
     response = requests.get(url, headers=headers)
     print(f"Status Code для сенсора: {response.status_code}")
